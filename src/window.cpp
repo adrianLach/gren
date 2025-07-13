9e25503d7226bb301e0de37c47a7dcc11217dedf
Adrian Lach <adrian.lach1999@gmail.com>
2025-07-13 12:36:35 +0200

    Adjust window creation settings to start in windowed mode and set anti-aliasing samples to 2

--- src/window.cpp
+++ src/window.cpp
@@ -27,9 +27,9 @@ Window::Window(int width, int height, const std::string& title)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE); // Start maximized
+
     // Anti-aliasing hint
-    // glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_SAMPLES, 2);
 
     // Create window
     m_window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
